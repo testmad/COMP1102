c2c2fb45bcb21a721141abc8ae518d9b718d7727
David Walker <testmad@thinstack.net>
2014-09-06 12:20:35 -0400

    Project 1 added.

---
+++ Project 1/project1.cpp
@@ -0,0 +1,23 @@
+/*
+David Walker
+Comp-1102 Project 1
+Assigned: 	09-05-2014
+Due:		09-17-2014
+*/
+
+#include <iostream.h>
+#include <math.h>
+
+int main(void)
+{
+
+	clear();
+	cout << "Please press enter.";
+	cin.get();
+	return 0;
+}
+
+void clear() {
+    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+    std::cout << "\x1B[2J\x1B[H";
+}
