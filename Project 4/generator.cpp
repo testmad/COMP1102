fa7ce2528cd281667c0a57001a80850dfaa16bf3
David Walker <testmad@thinstack.net>
2014-11-11 13:56:19 -0500

    project 4 finished

---
+++ Project 4/generator.cpp
@@ -0,0 +1,129 @@
+/*
+
+David Walker
+Comp-1102 Project 4 extra - data file generator
+Assigned: 	11-10-2014
+Due:		11-14-2014
+
+*/
+
+#include <iostream.h>
+#include <fstream.h>
+#include <math.h>
+#include <string.h>
+#include <iomanip.h>
+#include <time.h>
+
+const int FILENAME_SIZE = 255;
+
+void getFileName( char fileName[] );
+void openDataFile( ofstream& gradesDataFile, char fileName[] );
+int getRandomNumber();
+int getRandomGrade();
+
+int main(void)
+{
+            srand ( time(NULL));
+       
+    ofstream gradesDataFile;
+    char fileName[FILENAME_SIZE] = "\0";
+    
+    int numberRecords;
+    int numberGrades;
+
+    numberRecords = 0;
+    
+    getFileName( fileName );
+    
+    cout << "Creating file '" << fileName << "'." << endl;
+    
+    openDataFile( gradesDataFile, fileName );
+    
+    cout << "Number of records to generate: ";
+    cin >> numberRecords;
+    cout << endl;
+    
+    cout << "Number of grades per record to generate: ";
+    cin >> numberGrades;
+    cout << endl;
+    
+    gradesDataFile << numberGrades << '\n';
+    
+    for(int i = 1; i <= numberRecords; i++)
+    {
+        string firstName[10] = { "James", "John", "George", "Dave", "Steve", "Mary", "Jane", "Susie", "Kate", "Jean" };
+        string lastName[10] = { "Doe", "Jones", "Smith", "Chan", "Brown", "Johnson", "Garcia", "Miller", "Wilson", "Rodriguez" };
+
+        gradesDataFile << setw(25) << left << firstName[ getRandomNumber() ] + " " + lastName[ getRandomNumber() ];
+        
+        for(int i = 1; i <= numberGrades; i++)
+        {
+            gradesDataFile << " " << setw(3) << right << getRandomGrade();
+        }
+        
+        if(i < numberRecords)
+            gradesDataFile << '\n';
+        
+    }
+    cout << "Data file generation complete." << endl;
+    gradesDataFile.close();
+}
+
+void getFileName( char fileName[] )
+{
+    bool notValid = true;
+
+    while(notValid)
+    {    
+        cout << "Enter filename for generation: ";
+        cin.getline(fileName, FILENAME_SIZE, '\n');
+    
+        if(fileName[0] == '\0')
+        {
+            cout << "No filename provided." << endl;
+            continue;
+        }
+        else if(strstr(fileName, ".") == 0)
+        {
+            cout << "No file extension provided." << endl;
+            continue;
+        }
+        else
+        {
+            notValid = false;
+        }
+    }
+}
+
+void openDataFile( ofstream& gradesDataFile, char fileName[] )
+{
+    gradesDataFile.open(fileName);
+    
+    if(gradesDataFile.fail())
+    {
+        cout << "Error opening file '" << fileName << "'." << endl;
+        exit(1);
+    }
+    else
+    {
+        cout << "'" << fileName << "' has been created." << endl;
+    }
+    
+}
+
+int getRandomNumber()
+{
+    int randNumber;
+    randNumber = rand() % 10; 
+    return randNumber;
+}
+
+int getRandomGrade()
+{
+    int randGrade;
+    int max = 100;
+    int min = 72;
+    
+    randGrade = rand()%(max-min + 1) + min;
+    return randGrade;
+}
--- Project 4/project4.cpp
+++ Project 4/project4.cpp
@@ -16,11 +16,9 @@ const int FILENAME_SIZE = 255;
 void getFileName( char fileName[] );
 void openDataFile( ifstream& gradeDataFile, char fileName[] );
 void setGradeCount( ifstream& gradeDataFile, int& numGrades );
-void displayRecord( ifstream& gradeDataFile, int numGrades );
 void readRecord( ifstream& gradeDataFile, char studentName[], int studentGrades[], int numGrades );
 float avgForRecord( int studentGrades[], int numGrades );
-    float devForRecord( int studentGrades[], int numGrades );
-
+float devForRecord( int studentGrades[], int numGrades );
 
 int main(void)
 {
@@ -28,6 +26,8 @@ int main(void)
     char fileName[FILENAME_SIZE];
     int recordNumber;
     int numGrades;
+    char studentName[25];
+    int studentGrades[100];
     
     recordNumber = 1;
     
@@ -36,19 +36,16 @@ int main(void)
     getFileName( fileName );
     openDataFile( gradeDataFile, fileName );
     setGradeCount( gradeDataFile, numGrades );
-    
-    
-    
+
     while( gradeDataFile.peek() != EOF )
     {
-        cout << recordNumber << "  ";
-        displayRecord( gradeDataFile, numGrades );
-
-
-        
+        cout << setw(3) << recordNumber << "  ";
+        readRecord( gradeDataFile, studentName, studentGrades, numGrades );
+        cout << studentName << "  ";
+        cout << setw(6) << fixed << setprecision(2) << showpoint << avgForRecord( studentGrades, numGrades ) << "  ";
+        cout << setw(6) << fixed << setprecision(2) << showpoint << devForRecord( studentGrades, numGrades ) << endl;
+        recordNumber++;
     }
-    
-    
 }
 
 void getFileName( char fileName[] )
@@ -80,74 +77,61 @@ void getFileName( char fileName[] )
 
 void openDataFile( ifstream& gradeDataFile, char fileName[] )
 {
-    gradeDataFile.open(fileName);
+    gradeDataFile.open( fileName );
     
-    if(gradeDataFile.fail())
+    if( gradeDataFile.fail() )
     {
         cout << "Error opening file '" << fileName << "'." << endl;
-        exit(1);
+        exit( 1 );
     }
 }
 
 void setGradeCount( ifstream& gradeDataFile, int& numGrades )
 {
     gradeDataFile >> numGrades;
-    
+    gradeDataFile.get();
 }
 
-void displayRecord ( ifstream& gradeDataFile, int numGrades )
+void readRecord( ifstream& gradeDataFile, char studentName[], int studentGrades[], int numGrades )
 {
-    char studentName[25];
-    int studentGrades[100];
+    gradeDataFile.get( studentName, 25 );
     
-    readRecord( gradeDataFile, studentName, studentGrades, numGrades );
-    
-    cout << studentName << " " << avgForRecord( studentGrades, numGrades) << " " << devForRecord(studentGrades, numGrades) << endl;
-    
-    
-}
-
-void readRecord( ifstream& gradeDataFile, char studentName[], int studentGrades[], int numGrades)
-{
-    gradeDataFile.get( studentName, 25);
-    
-    for( int i = 0; i <= numGrades; i++)
-        cin >> studentGrades[i];
+    for( int i = 0; i < numGrades; i++ )
+        gradeDataFile >> studentGrades[i];
     
     gradeDataFile.get();
 }
 
 float avgForRecord( int studentGrades[], int numGrades )
 {
-    float smallestGrade = 0;
-    float sum = 0;
-    for( int i = 0; i <= numGrades; i++)
+    float smallestGrade;
+    float sum;
+    
+    smallestGrade = studentGrades[0];
+    
+    for( int i = 0; i < numGrades; i++ )
     {
-        sum = sum + studentGrades[i];
         if( studentGrades[i] < smallestGrade )
             smallestGrade = studentGrades[i];
+        
+        sum = sum + studentGrades[i];
     }
     
-    return (sum - smallestGrade)/(numGrades - 1);
+    return ( sum - smallestGrade ) / ( numGrades - 1 );
 }
 
 float devForRecord( int studentGrades[], int numGrades )
 {
-    float avg = 0.0;
+    float avg;
     float sum = 0;
-    for( int i = 0; i <= numGrades; i++)
-    {
+    for( int i = 0; i < numGrades; i++ )
         sum = sum + studentGrades[i];
-    }
     
-    avg = sum/numGrades;
+    avg = sum / numGrades;
+    sum = 0;
     
-    for( int i = 0; i <= numGrades; i++)
-    {
-        sum = sum + (studentGrades[i] - avg)*(studentGrades[i] - avg);
-    }
+    for( int i = 0; i < numGrades; i++)
+        sum = sum + ( studentGrades[i] - avg ) * ( studentGrades[i] - avg );
     
-    return sqrt( sum/numGrades );
+    return sqrt( sum / numGrades );
 }
-
-
